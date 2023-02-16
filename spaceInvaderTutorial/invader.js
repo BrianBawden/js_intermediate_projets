731fe0b8e7f7e61d61d3874687953ea475944722
Brian Bawden <brianbawden001@gmail.com>
2023-02-16 11:16:42 -0500

    gid set up for gameboard.

--- spaceInvaderTutorial/invader.js
+++ spaceInvaderTutorial/invader.js
@@ -1,3 +1,7 @@
 
-const gameB = document.querySelector("#gameBoard");
+const grid = document.querySelector(".grid");
 
+for (let i = 0; i < 156; i++){
+    const square = document.createElement("div")
+    grid.appendChild(square) 
+}
